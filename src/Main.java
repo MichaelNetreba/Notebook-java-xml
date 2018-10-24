a8b317573cb8516c11716fde2c9ffecdef97ebf9
Michael Netreba <mike_netreba@mail.ru>
2018-10-24 23:36:09 +0300

    ..

--- src/Main.java
+++ src/Main.java
@@ -36,31 +36,92 @@ public class Main {
         String n=reader.readLine();
         if (n.equals("1") || n.equals("2")){
 switch(n){
-    case "1" :
+    case "1" : // First branch;
         System.out.println("Will this User be Manager or Worker?"+ '\n'+"1.Manager" +'\n'+"2.Worker");
             String n2 = reader.readLine();
             if (n2.equals("1")|| n2.equals("2")){
                 switch(n2){
                     case "1" :
-                        System.out.println("Input first_name,last_name, phone, year(number), department");
+                        System.out.println("Input first_name,last_name, phone, year(number), department"+'\n'+"(Press Enter after every input.)");
                         addManager(reader.readLine(),reader.readLine(),reader.readLine(),Integer.parseInt(reader.readLine()), reader.readLine());
 
                         System.out.println("Manager has been added");
                         break;
                     case "2" :
-                        System.out.println("Input first_name,last_name, phone, year(number), name of Manager");
+                        System.out.println("Input first_name,last_name, phone, year(number), name of Manager"+'\n'+"(Press Enter after every input.)");
                         addWorker(reader.readLine(),reader.readLine(),reader.readLine(),Integer.parseInt(reader.readLine()),reader.readLine());
                         System.out.println("Worker has been added");
                 }
 
+            } else { System.out.println("Error Input. Try again"); }
+            break;
+
+        case "2" : // Second branch;
+        System.out.println("What would you like to do:"+'\n'+ "1.Find Manager by First_Name"+'\n'+ "2.Find Manager by Last_Name"+'\n'
+                +"3.Find Manager by Phone_Number"+'\n'+"4.Find Worker by First_Name"+'\n'+"5.Find Worker by Last_Name"+'\n'+"6.Find Worker by Phone_Number"+'\n'
+                +"7.Sort Users by Last_Name"+'\n'+"8.Sort Users by Birthday(Year)"+'\n'+"9.Remove Manager by First_Name"+'\n'+"10.Remove Manager by Last_Name"+'\n'
+                +"11.Remove Worker by First_Name"+'\n'+"12.Remove Worker by Last_Name");
+        String n3 = reader.readLine();
+            if (n3.equals("1") || n3.equals("2") || n3.equals("3")|| n3.equals("4")|| n3.equals("5")||
+            n3.equals("6")||n3.equals("7")||n3.equals("8")||n3.equals("9")||n3.equals("10")||n3.equals("11")||n3.equals("12")) {
+             switch(n3) {
+                 case "1" :
+                     System.out.println("Write the manager's first name.Press Enter.");
+                     getManagerByName(reader.readLine());
+                     break;
+                 case "2" :
+                     System.out.println("Write the manager's last name. Press Enter.");
+                     getManagerByLastname(reader.readLine());
+                     break;
+                 case "3" :
+                     System.out.println("Write the manager's phone number. Press Enter");
+                     getManagerByPhone(reader.readLine());
+                     break;
+                 case "4" :
+                     System.out.println("Write the worker's first name.Press Enter.");
+                     getWorkerByName(reader.readLine());
+                     break;
+                 case "5" :
+                     System.out.println("Write the worker's last name.Press Enter.");
+                     getWorkerByLastname(reader.readLine());
+                     break;
+                 case "6" :
+                     System.out.println("Write the worker's phone number.Press Enter.");
+                     getWorkerByPhone(reader.readLine());
+                     break;
+                 case "7" :
+                     sortByLastName();
+                     System.out.println("Sort has been done successfully.");
+                     break;
+                 case "8" :
+                     sortByBirth();
+                     System.out.println("Sort has been done successfully.");
+                     break;
+                 case "9" :
+                     removeManagerByName(reader.readLine());
+                     System.out.println("Manager has been removed");
+                     break;
+                 case "10" :
+                     removeManagerByLastname(reader.readLine());
+                     System.out.println("Manager has been removed");
+                     break;
+                 case "11" :
+                     removeWorkerByName(reader.readLine());
+                     System.out.println("Worker has been removed");
+                     break;
+                 case "12" :
+                     removeWorkerByLastname(reader.readLine());
+                     System.out.println("Worker has been removed");
+                     break;
+
+
+
+             }
 
             } else { System.out.println("Error Input. Try again"); }
 
 
-        break;
-
-        case "2" :
-        System.out.println("What would you like to do: ??? "); }
+    }
 
         } else { System.out.println("Error Input. Try again"); }
 
